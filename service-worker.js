6c46bd1db4f8751533cc68e63ca1c33e7f32dfdd
Geiza Leite <geiza.leite.ti@gmail.com>
2026-06-10 21:59:20 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,25 @@
-self.addEventListener("install", () => {
-    console.log("Service Worker instalado");
+const CACHE_NAME = "controle-financeiro-v1";
+
+const ARQUIVOS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(ARQUIVOS))
+    );
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+});
